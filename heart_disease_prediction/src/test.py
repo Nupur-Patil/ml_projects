ad0adfac67856db95bf0d1a4bdee219c95bf9fe8
Nupur-Patil <patilnupur5@gmail.com>
2025-11-15 07:00:57 -0600

    11/15

---
+++ heart_disease_prediction/src/test.py
@@ -0,0 +1,7 @@
+class ModelMetrics(true_labels, pred_labels)
+    
+    def __init__(self, data):
+        self.data = data
+        print('Data initialised!')
+        print(f'Data list is {data})
+    def calculate_precision()
